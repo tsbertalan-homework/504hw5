3438bc8a29706023f1d7589385d334009e657c79
Tom S. Bertalan <tom@tombertalan.com>
2012-11-18 20:49:45 -0500

    dy/dtau did have multiple roots, I just needed the right parameter combination.

--- hw5_2.py
+++ hw5_2.py
@@ -1,38 +1,34 @@
 import matplotlib.pyplot as plt
 import numpy as np
-Sf = lambda y,g: y / (1 + y / g)
-dyf = lambda y, a, b, g: -y + np.exp(Sf(y,g)) * (a * b * g - a * y)
+from finder import findroots
+
+f = lambda y, g: np.exp(y / (1 + y / g))
+dyf = lambda y, a, b, g: f(y, g) * (a * b * g - a * y) - y
 dxf = lambda y, a, b, g: dyf(y, a, b, g) / b / g
-ddyf = lambda y, a, b, g: (a*b*g - a*y)*(g*(y/g+1)+y)/g/(y/g+1)*np.exp(Sf(y,g)) - a*np.exp(Sf(y,g))  -1
-J0condf = lambda x, a, b, g: -b*g*x**2 + b*g*x - 1
 
-yl = list(np.arange(0,1,.01))
-a = 1.5
-b = 0.0
-g = 1.5
-trial_values = [1, 3]
-maxinit = 0
-maxparams = (0, 0, 0)
+yl = list(np.arange(0, 10, 0.01))
+a = 0.005
+b = 3
+g = 10
 
-#for a in trial_values:
-for b in trial_values:
-    for g in trial_values:
-        label = r'$\alpha=%i$, $\beta=%i$, $\gamma=%i$'%(a,b,g)
-        dxl = [dxf(y, a, b, g) for y in yl]
-        ddyl = [ddyf(y,a,b,g) for y in yl]
-        dyl = [dyf(y, a, b, g) for y in yl]
-        j0condl = [J0condf(x, a, b, g) for x in yl]
-        plt.plot(yl, j0condl, label=label)
-        if dyl[0] > maxinit:
-            maxinit = dyl[0]
-            maxparams = (a, b, g)
-        if ddyl[0] > 0 and dyl[0] == 0:
-            print 'ddyl[0] =', ddyl[0], '> 0', 'for (a b g) = (', a,b,g, ')'
-        absdyl = [abs(dy) for dy in dyl]
-        zeroest = min(absdyl)
-        ys = yl[absdyl.index(zeroest)]
-        print 'ys =', ys, 'for', a, b, g
-print 'highest initial rate was', maxinit, 'at', a, b, g
+labelx = r'concentration rate-of-change, $dx/d\tau$'
+labely = r'temperature rate-of-change, $dy/d\tau$'
+dxl = [dxf(y, a, b, g) for y in yl]
+dyl = [dyf(y, a, b, g) for y in yl]
+
+plt.plot(yl, dxl, 'k--', label=labelx)
+plt.plot(yl, dyl, 'k-', label=labely)
+roots = findroots(yl, dyl)
+print 'roots are', roots
+plt.scatter(roots, [0]*len(roots))
+for root in roots:
+    plt.annotate(r'$y_s=%.3f$' % root, xy=(root, 0), xytext=(root, -0.2))
 plt.axhline(color='black')
+plt.xlabel(r'$y$')
+plt.ylabel(r'$dy/d\tau$ or $dx/d\tau$')
+plt.xlim((0, 8))
+plt.ylim((-2, 2))
 plt.legend()
+plt.title('Multiple CSTR Steady States \n'\
+          + r'$\alpha=%.3f$, $\beta=%.0f$, $\gamma=%.0f$' % (a, b, g))
 plt.show()
