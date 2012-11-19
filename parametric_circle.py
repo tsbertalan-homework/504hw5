3b45a4f694efa55d636d0d882f4ce55888b5e822
Tom S. Bertalan <tom@tombertalan.com>
2012-11-18 19:56:47 -0500

    This is incredibly silly.

---
+++ parametric_circle.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+pi = np.pi
+
+tmin = 0.0
+tmax = 2 * pi
+dt = 0.01
+
+tl = list(np.arange(tmin, tmax, dt))
+xl = [np.cos(t) for t in tl]
+yl = [np.sin(t) for t in tl]
+
+fig = plt.figure(1, figsize=(12, 6))
+ax1 = fig.add_subplot(1, 2, 1)
+ax2 = fig.add_subplot(1, 2, 2)
+ax1.plot(tl, xl, label=r'$x=cos(t)$')
+ax1.plot(tl, yl, label=r'$y=sin(t)$')
+ax2.plot(xl, yl, label=r'$y$ vs $x$')
+
+ax1.set_xlim((tmin, tmax))
+ax2.set_xlabel(r'$x$')
+ax2.set_ylabel(r'$y$')
+ax1.set_title('Dafuq is this?')
+ax2.set_title('It\'s a circle, fuckass.')
+
+fig.suptitle('Parametric Plots are good, M\'kay?')
+ax1.legend()
+plt.savefig('parametric.png')
+plt.show()
