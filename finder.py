fb6ee44cfb1c25909242063be3cdcbe79286207f
Tom S. Bertalan <tom@tombertalan.com>
2012-11-18 22:12:23 -0500

    characterizing steady-states

--- finder.py
+++ finder.py
@@ -1,15 +1,16 @@
+def isPositive(value):
+    if value > 0:
+        return True
+    else:
+        return False
+
 def findroots(x, dx):
     '''
     Search forward from 0, recording places where sign changes.
     '''
-    def ispositive(value):
-        if value > 0:
-            return True
-        else:
-            return False
     indices = []
     for i in range(1, len(dx)):
-        if ispositive(dx[i]) != ispositive(dx[i-1]):
+        if isPositive(dx[i]) != isPositive(dx[i-1]):
             indices.append(i)
     roots = [x[i] for i in indices]
     return roots
